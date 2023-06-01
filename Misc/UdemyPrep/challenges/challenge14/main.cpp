869f94e17718517d4c40b93e6460f89d5b9bf77f
Cownic <95520066+Cownic@users.noreply.github.com>
2023-06-01 09:23:09 +0000

    Challenge14 template done

---
+++ Misc/UdemyPrep/challenges/challenge14/main.cpp
@@ -0,0 +1,19 @@
+/*
+    Overload the following operators in Mystring class
+
+    unary minus - : return the lowercase version of the object's string
+    ==            : return true if the two strings are equal
+    !=            : return true if the two strings are not equal
+    <             : return true if the lhs string is lexically less than the rhs string
+    >             : return true if the lhs string is lexically more than the rhs string 
+    +             : concat lhs with rhs and return the combined string in an object
+    +=            : concat the ehs string to the lhs string and store result in lhs object
+*/
+
+#include <iostream>
+#include "MyString.h"
+
+int main()
+{
+    return 0;
+}
